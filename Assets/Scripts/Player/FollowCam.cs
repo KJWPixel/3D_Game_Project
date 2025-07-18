647bfe3355916c73a767800c7fcef8743a6addd6
DESKTOP-61JG5QH\owner <kjy87437902@gmail.com>
2025-07-18 17:02:51 +0900

    1인칭 FPS 보완 및 Character Controller 사용

--- Assets/Scripts/Player/FollowCam.cs
+++ Assets/Scripts/Player/FollowCam.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 //1인칭, 3인칭 스위칭하는 기능 및 Offset값을 가지고 세부조정가능 
 public class FollowCam : MonoBehaviour
 {
-    [Header("카메라 위치")]
-    [SerializeField] Transform FollowTransform;
-
+    [Header("카메라 위치, 후방거리 ")]
+    [SerializeField] GameObject LookTransform = null;
+    [SerializeField] float RearBackDistance = 0f;
+    
     [Header("카메라 위치 오프셋")]
-    [SerializeField] Vector3 Offset;
+    [SerializeField] Vector3 Offset = Vector3.zero;
 
     [Header("카메라 1,3 인칭 스위치")]
     [SerializeField] bool firstPerson = true;
 
-
+    float MouseXValue = 0f;
+    float MouseYValue = 0f;
 
     void Start()
     {
-        this.transform.position = FollowTransform.position;
+        //Offset = new Vector3(0f, 0f, -1f * RearBackDistance);
+
+    }
+
+    private void Update()
+    {
+        float MouseX = Input.GetAxisRaw("Mouse X");
+        float MouseY = Input.GetAxisRaw("Mouse Y");
+
+        MouseXValue = MouseXValue + MouseX;
+        MouseYValue = MouseYValue + MouseY * (-1.0f);
+        //화면에서 회전을 반전시키기 위해 -1.0f을 곱함
+
+        this.transform.rotation = Quaternion.Euler(MouseYValue, MouseXValue, 0f);
     }
 
 
     private void LateUpdate()
     {
-        transform.position = FollowTransform.position + Offset;
-        transform.rotation = FollowTransform.rotation;
+        //transform.position = LookTransform.position + Offset;
+        //transform.rotation = LookTransform.rotation;
+        this.transform.position = LookTransform.transform.position + this.transform.rotation * Offset;
     }
+
 }
--- Assets/Scripts/Player/Player Controller.cs
+++ Assets/Scripts/Player/Player Controller.cs
@@ -6,66 +6,92 @@ using UnityEngine;
 //X축 위 아래, Y축 좌 우, Z 좌 우 
 public class PlayerController : MonoBehaviour
 {
-    [Header("플레이어 카메라 위치")]
-    [SerializeField] Transform Head;
+    //ChracterController 컴포넌트 사용
+    [SerializeField] CharacterController CharController = null;
 
-    [Header("플레이어 이동")]
+    [Header("플레이어 이동속도 값")]
     [SerializeField] float MoveSpeed = 0f;
     [SerializeField] float JumpForce = 0f;
 
-    [Header("플레이어 마우스 회전 감도")]
-    [SerializeField] float lookSensitivity = 2f;
-    private float rotX = 0f;
+    [Header("플레이어 속도")]
+    [SerializeField] Vector3 VelocityValue = Vector3.zero;
 
-    Rigidbody rigid;
+    [Header("중력 가속도")]
+    [SerializeField] float Gravity = -9.81f;
+
+    [Header("플레이어 마우스 커서 제어")]
+    [SerializeField] CursorLockMode CoursorLock = CursorLockMode.None;
 
     private void Awake()
     {
-        rigid = GetComponentInChildren<Rigidbody>();
+
     }
     void Start()
     {
     }
-
+    
 
     void Update()
     {
-        Look();
+        //Look();
         Move();
         Jump();
     }
 
-    private void Look()
-    {
-        //Mouse X,Y: 매 프레임마다 마우스의 움직임 정도
-        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
+    //private void Look()
+    //{
+    //    //Mouse X,Y: 매 프레임마다 마우스의 움직임 정도
+    //    float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
+    //    float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
 
-        // 플레이어 전체를 좌우 회전 (Y축)
-        transform.Rotate(Vector3.up * mouseX);
+    //    // 플레이어 전체를 좌우 회전 (Y축)
+    //    transform.Rotate(Vector3.up * mouseX);
 
-        // Head만 상하 회전 (X축)
-        rotX -= mouseY;
-        rotX = Mathf.Clamp(rotX, -90f, 90f);
-        Head.localRotation = Quaternion.Euler(rotX, 0f, 0f);
-    }
+    //    // Head만 상하 회전 (X축)
+    //    rotX -= mouseY;
+    //    rotX = Mathf.Clamp(rotX, -90f, 90f);
+    //    Head.localRotation = Quaternion.Euler(rotX, 0f, 0f);
+    //}
 
     private void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if(CharController.isGrounded)//캐릭터 컨트롤러가 지면에 닿았다면
+        {
+            //방향축 입력
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 Move = transform.right * x + transform.forward * z;
-        transform.position += Move * MoveSpeed * Time.deltaTime;
+            //Vector3 move = transform.right * x + transform.forward * z;
+            //입력값을 기반으로 xz평면에서의 속도 결정
+            Vector3 velocity = new Vector3(x, 0f, z);
+            velocity = CharController.transform.TransformDirection(velocity);
+            VelocityValue = velocity.normalized * MoveSpeed;
+        }
+        else//캐릭터 컨트롤러가 지면에 닿지 않았다면(공중이라면)
+        {
+            //현재 속도 = 이전속도 + 가속도 * 간격
+            VelocityValue.y = VelocityValue.y + Gravity * Time.deltaTime;
+        }
+
+        //CharacterController에서 제공하는 Move함수를 사용
+        CharController.Move(VelocityValue * Time.deltaTime);
+
+
+        //카메라가 바라보는 방향으로 선회
+        Vector3 tDir = Camera.main.transform.forward;
+        tDir.y = 0f;
+        //카메라 전방 방향의 임의의 지점을 구한다.
+        Vector3 tLookAtPosition = this.transform.position + tDir;
+        //카메라 전방 방향의 임의의 지점을 바라보게 한다.
+        this.transform.LookAt(tLookAtPosition);
+
     }
 
     private void Jump()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            float VerticalVelocity = 0f;
-            VerticalVelocity += JumpForce;
-            rigid.velocity = new Vector3 (0f, VerticalVelocity, 0f);
+            VelocityValue.y = JumpForce;
         }
     }
 
